24e46a84881e169cae483c63f84202c928981c1d
Marcos.Pereira <Marcos.Pereira@WIN-QCA45U55BBM>
2023-05-24 10:01:42 -0300

    Comit 1

--- Program.cs
+++ Program.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using SAPbobsCOM;
+using SAPbouiCOM;
+
 
 namespace RevisaoMarcos
 {
     internal static class Program
     {
+        public static Application SBOAplication = null; // tipo de  variavel Aplication
         /// <summary>
         /// Ponto de entrada principal para o aplicativo.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            SboGuiApi sboGuiApi = null; // declarando variaveis de conexão
+            string sConnection = null; // declarando variaveis de conexão
+
+            sboGuiApi = new SboGuiApi();
+
+            sConnection = "0030002C0030002C00530041005000420044005F00440061007400650076002C0050004C006F006D0056004900490056";
+
         }
     }
 }
